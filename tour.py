11c1fa63b4de57d301b6c5501682d372aef4e568
kian chachi <chachi@kians-MacBook-Pro.local>
2019-10-30 10:32:37 -0700

    first

---
+++ tour.py
@@ -0,0 +1,140 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import time
+
+
+
+x = 0
+y = 120
+z = 0
+
+
+mc.player.setTilePos(x, y, z)
+
+
+
+time.sleep(3)
+
+
+x = 20
+y = 80
+z = 20
+
+
+mc.player.setTilePos(x, y, z)
+
+
+time.sleep(3)
+
+
+x = 20
+y = 100
+z = 20
+
+
+mc.player.setTilePos(x, y, z)
+
+
+
+
+time.sleep(3)
+
+x = 0
+y = 120
+z = 0
+
+
+mc.player.setTilePos(x, y, z)
+
+
+
+time.sleep(3)
+
+
+x = 20
+y = 80
+z = 20
+
+
+mc.player.setTilePos(x, y, z)
+
+
+time.sleep(3)
+
+
+x = 20
+y = 100
+z = 20
+
+
+mc.player.setTilePos(x, y, z)
+
+time.sleep(3)
+
+
+x = 0
+y = 120
+z = 0
+
+
+mc.player.setTilePos(x, y, z)
+
+
+
+time.sleep(3)
+
+
+x = 20
+y = 80
+z = 20
+
+
+mc.player.setTilePos(x, y, z)
+
+
+time.sleep(3)
+
+
+x = 20
+y = 100
+z = 20
+
+
+mc.player.setTilePos(x, y, z)
+
+
+
+
+time.sleep(3)
+
+x = 0
+y = 120
+z = 0
+
+
+mc.player.setTilePos(x, y, z)
+
+
+
+time.sleep(3)
+
+
+x = 20
+y = 80
+z = 20
+
+
+mc.player.setTilePos(x, y, z)
+
+
+time.sleep(3)
+
+
+x = 20
+y = 100
+z = 20
+
+
+mc.player.setTilePos(x, y, z)
+
+time.sleep(3)
